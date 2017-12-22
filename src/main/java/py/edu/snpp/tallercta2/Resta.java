e786cb3834ff8e29952ed83cc669dce3e47cec9c
Cristhian <Cristhian@192.168.2.168>
2017-12-22 09:51:18 -0300

    TGIT-4 Clase con metodo calcular, que resta dos numeros

---
+++ src/main/java/py/edu/snpp/tallercta2/Resta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.edu.snpp.tallercta2;
+
+/**
+ *
+ * @author Cristhian
+ */
+public class Resta {
+    //recibir dos valores X e Y
+    public static double calcular(double x, double y) {
+        double resultado = x - y;
+        return resultado;
+    }
+}
