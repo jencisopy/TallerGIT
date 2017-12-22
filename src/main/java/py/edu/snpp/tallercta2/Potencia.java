be0b25d8421c4455b87979cbfbc60bf5deecb7d9
Jorge Enciso <jorge.enciso.r@gmail.com>
2017-12-21 20:59:42 -0400

    Clase potencia

---
+++ src/main/java/py/edu/snpp/tallercta2/Potencia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.edu.snpp.tallercta2;
+
+/**
+ *
+ * @author Jorge Enciso
+ */
+public class Potencia {
+    public static double elevar(double base, int exponente){
+        double resultado = (double) Math.pow(base, exponente);
+        return resultado;
+    }
+}
---
+++ src/test/java/py/edu/snpp/tallercta2/PotenciaTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.edu.snpp.tallercta2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge Enciso
+ */
+public class PotenciaTest {
+    
+    public PotenciaTest() {
+    }
+
+    /**
+     * Test of elevar method, of class Potencia.
+     */
+    @Test
+    public void testElevar() {
+        System.out.println("elevar");
+        double base = 4.0;
+        int exponente = 2;
+        double expResult = 16.0;
+        double result = Potencia.elevar(base, exponente);
+        assertEquals(expResult, result, 0.0);
+        //fail("The test case is a prototype.");
+    }
+    
+}
